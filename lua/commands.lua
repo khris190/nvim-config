81a018e2b63a9a18ef7423b86ce937c1a628325c
khris190 <khris10@interia.pl>
2024-04-05 00:22:31 +0200

    third restart with lazyVim as a default

--- lua/commands.lua
+++
@@ -1,8 +0,0 @@
-local utils = require "utils"
-
-vim.api.nvim_create_autocmd("OptionSet", {
-  pattern = "background",
-  callback = function()
-    vim.cmd("Catppuccin " .. (vim.v.option_new == "light" and "latte" or "mocha"))
-  end,
-})
