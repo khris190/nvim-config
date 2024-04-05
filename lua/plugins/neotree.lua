834879a2c354c6f9a62247f1f1b86d5e299f9258
khris190 <khris10@interia.pl>
2024-04-05 08:59:58 +0200

    remove toggle from neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = false, dir = LazyVim.root() })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = false, dir = vim.uv.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+  },
+}
