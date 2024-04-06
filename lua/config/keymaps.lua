3b3c15f5d813b827cee2a2d57fe67e153b589b97
khris190 <khris10@interia.pl>
2024-04-06 22:31:48 +0200

    feat: fix leader tab keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,22 @@ vim.api.nvim_create_autocmd("BufEnter", {
 vim.keymap.set("n", "<A-s>", function()
   vim.cmd("w")
 end)
+
+-- tabs
+vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+vim.keymap.set("n", "<leader><tab>d", function()
+  local bd = require("mini.bufremove").delete
+  if vim.bo.modified then
+    local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+    if choice == 1 then -- Yes
+      vim.cmd.write()
+      bd(0)
+    elseif choice == 2 then -- No
+      bd(0, true)
+    end
+  else
+    bd(0)
+  end
+end, { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader><tab>[", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Tab" })
+vim.keymap.set("n", "<leader><tab>]", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Tab" })
