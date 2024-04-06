08546a45313d313757ac6ba698755a56882452af
khris190 <khris10@interia.pl>
2024-04-06 21:46:52 +0200

    feat: add test keymap only for .rs files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.rs" },
+  callback = function(ev)
+    vim.schedule(function()
+      vim.api.nvim_buf_set_keymap(0, "n", "<A-r>", "", {
+        callback = function()
+          -- vim.cmd("RustRun")
+          print("Alt in Rust file")
+        end,
+      })
+    end)
+  end,
+})
