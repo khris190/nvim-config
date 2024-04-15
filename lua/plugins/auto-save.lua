7fd3a2b30eb16bf48657b011e320f3bd5a9c1e1e
khris190 <k.para@contina.pl>
2024-04-15 12:57:01 +0200

    feat: add autosave on buffer change and after insert

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,12 @@
+return {
+  "okuuva/auto-save.nvim",
+  event = { "InsertLeave", "WinLeave" }, -- optional for lazy loading on trigger events
+
+  opts = {
+    trigger_events = { -- See :h events
+      immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+      defer_save = { "InsertLeave" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+      cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+    },
+  },
+}
