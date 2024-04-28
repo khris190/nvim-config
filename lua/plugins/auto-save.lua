b87c4763116d9243bf51bf4573bedc72d7465803
khris190 <khris10@interia.pl>
2024-04-28 13:07:08 +0200

    feat: remove autosave on InsertLeave

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     trigger_events = { -- See :h events
       immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
-      defer_save = { "InsertLeave" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+      defer_save = {}, -- vim events that trigger a deferred save (saves after `debounce_delay`)
       cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
     },
   },
