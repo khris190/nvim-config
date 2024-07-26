96e74f753f2121009bc3070d6dc5f7323c0fcb0c
khris190 <k.para@contina.pl>
2024-07-26 22:09:05 +0200

    fix: change folding behaviour

--- lua/plugins/folds.lua
+++ lua/plugins/folds.lua
@@ -3,4 +3,13 @@ return {
   dependencies = {
     "kevinhwang91/promise-async",
   },
+  opts = {
+    provider_selector = function(bufnr, filetype, buftype)
+      -- if you prefer treesitter provider rather than lsp,
+      -- return ftMap[filetype] or {'treesitter', 'indent'}
+      return { "treesitter", "indent" }
+
+      -- refer to ./doc/example.lua for detail
+    end,
+  },
 }
