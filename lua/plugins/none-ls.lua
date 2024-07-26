97e21c04e3d45b11b21930d03e3125d720659f41
khris190 <k.para@contina.pl>
2024-07-26 22:10:14 +0200

    feat: update none-ls commands to be project local

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,21 +3,40 @@ return {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      local composer_globa_dir = vim.fn.expand("$HOME/.config/composer")
-      local composer_global_bin_dir = composer_globa_dir .. "/vendor/bin"
+      local composer_dir = vim.fn.getcwd()
+      local composer_bin_dir = composer_dir .. "/vendor/bin"
 
       vim.list_extend(opts.sources, {
         nls.builtins.formatting.phpcsfixer.with({
-          command = composer_global_bin_dir .. "/php-cs-fixer",
+          command = composer_bin_dir .. "/php-cs-fixer",
           extra_args = {
-            "--config",
-            composer_globa_dir .. "/.php_cs_fixer.php",
+            "--config=" .. composer_dir .. "/.php-cs-fixer.dist.php",
           },
         }),
-        nls.builtins.diagnostics.phpstan.with({
-          command = composer_global_bin_dir .. "/phpstan",
-          extra_args = { "-l", "max" },
+        nls.builtins.diagnostics.phpcs.with({
+          command = composer_bin_dir .. "/phpcs",
+          extra_args = { "--standard=" .. composer_dir .. "/.phpcs.xml" },
         }),
+      })
+    end,
+    config = function()
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      require("null-ls").setup({
+        -- you can reuse a shared lspconfig on_attach callback here
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
       })
     end,
   },
