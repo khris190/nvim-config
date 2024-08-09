730cce18cc858be4725da25f341699324ec4fb98
khris190 <khris10@interia.pl>
2024-08-09 10:53:14 +0200

    feat: change lazyload of phpeasynvim

--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -1,23 +1,25 @@
 return {
   "ta-tikoma/php.easy.nvim",
-  config = true,
-  keys = {
-    { "-b", "<CMD>PHPEasyDocBlock<CR>" },
-    { "-r", "<CMD>PHPEasyReplica<CR>" },
-    { "-c", "<CMD>PHPEasyCopy<CR>" },
-    { "-d", "<CMD>PHPEasyDelete<CR>" },
-    { "-uu", "<CMD>PHPEasyRemoveUnusedUses<CR>" },
-    { "-e", "<CMD>PHPEasyExtends<CR>" },
-    { "-i", "<CMD>PHPEasyImplements<CR>" },
-    { "-ii", "<CMD>PHPEasyInitInterface<CR>" },
-    { "-ic", "<CMD>PHPEasyInitClass<CR>" },
-    { "-iac", "<CMD>PHPEasyInitAbstractClass<CR>" },
-    { "-it", "<CMD>PHPEasyInitTrait<CR>" },
-    { "-ie", "<CMD>PHPEasyInitEnum<CR>" },
-    { "-c", "<CMD>PHPEasyAppendConstant<CR>", mode = { "n", "v" } },
-    { "-p", "<CMD>PHPEasyAppendProperty<CR>", mode = { "n", "v" } },
-    { "-m", "<CMD>PHPEasyAppendMethod<CR>", mode = { "n", "v" } },
-    { "-_", "<CMD>PHPEasyAppendConstruct<CR>" },
-    { "-a", "<CMD>PHPEasyAppendArgument<CR>" },
-  },
+  config = function()
+    -- easy php
+    vim.keymap.set("n", "-b", "<CMD>PHPEasyDocBlock<CR>")
+    vim.keymap.set("n", "-r", "<CMD>PHPEasyReplica<CR>")
+    vim.keymap.set("n", "-c", "<CMD>PHPEasyCopy<CR>")
+    vim.keymap.set("n", "-d", "<CMD>PHPEasyDelete<CR>")
+    vim.keymap.set("n", "-uu", "<CMD>PHPEasyRemoveUnusedUses<CR>")
+    vim.keymap.set("n", "-e", "<CMD>PHPEasyExtends<CR>")
+    vim.keymap.set("n", "-i", "<CMD>PHPEasyImplements<CR>")
+    vim.keymap.set("n", "-ii", "<CMD>PHPEasyInitInterface<CR>")
+    vim.keymap.set("n", "-ic", "<CMD>PHPEasyInitClass<CR>")
+    vim.keymap.set("n", "-iac", "<CMD>PHPEasyInitAbstractClass<CR>")
+    vim.keymap.set("n", "-it", "<CMD>PHPEasyInitTrait<CR>")
+    vim.keymap.set("n", "-ie", "<CMD>PHPEasyInitEnum<CR>")
+    vim.keymap.set({ "n", "v" }, "-c", "<CMD>PHPEasyAppendConstant<CR>")
+    vim.keymap.set({ "n", "v" }, "-p", "<CMD>PHPEasyAppendProperty<CR>")
+    vim.keymap.set({ "n", "v" }, "-m", "<CMD>PHPEasyAppendMethod<CR>")
+    vim.keymap.set("n", "-_", "<CMD>PHPEasyAppendConstruct<CR>")
+    vim.keymap.set("n", "-a", "<CMD>PHPEasyAppendArgument<CR>")
+    require("php-easy-nvim").setup({})
+  end,
+  ft = { "php" },
 }
