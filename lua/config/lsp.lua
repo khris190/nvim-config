ce4e83664d84ceb47d98083c4d16e0d64bf12f68
khris190 <khris10@interia.pl>
2025-05-06 23:11:49 +0200

    ffeat: update mehod to get vue install path to new mason version

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,6 @@
 -- If you are using mason.nvim, you can get the ts_plugin_path like this
 local mason_registry = require("mason-registry")
-local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+local vue_language_server_path = vim.fn.expand("$MASON/packages/vue-language-server")
   .. "/node_modules/@vue/language-server"
 
 local lspconfig = require("lspconfig")
